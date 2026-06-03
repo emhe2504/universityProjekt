84a616692f106f9798e1b789e42be5fe13028603
Emma Heikkinen <emhe2504@student.miun.se>
2026-06-03 13:09:24 +0200

    Rubriker, interface och startade upp service

---
+++ src/app/models/courseInt.ts
@@ -0,0 +1,8 @@
+export interface Course {
+    id: number;
+    courseCode: string;
+    courseName: string;
+    points: number;
+    subject: string;
+    syllabus: string;
+}
--- src/app/services/courses.ts
+++ src/app/services/courses.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Courses {}
+export class Courses {
+
+  private url: string = "https://matdah.github.io/DT208G---Programmering-i-TypeScript/Moment%205%20-%20Projekt/miun_courses.json";
+
+  //injicerar HttpClient
+  http = inject(HttpClient);
+}
