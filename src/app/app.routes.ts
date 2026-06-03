a5b7886e622f818b80734e99dc61c2ee5ee37325
Emma Heikkinen <emhe2504@student.miun.se>
2026-06-03 12:50:39 +0200

    Routes, httpclient

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Courses } from './courses/courses';
+import { Schedule } from './schedule/schedule';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "Home", component: Home},
+    { path: "", redirectTo: "Home", pathMatch: "full"},
+    { path: "Courses", component: Courses},
+    { path: "Schedule", component: Schedule}
+];
