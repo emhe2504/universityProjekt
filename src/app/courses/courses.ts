7b8d28d04254d683930051b3859a594cedd552c6
Emma Heikkinen <emhe2504@student.miun.se>
2026-06-05 13:13:31 +0200

    Kunna sortera kurser på kurskod, kursnamn, poäng, ämne

--- src/app/courses/courses.ts
+++ src/app/courses/courses.ts
@@ -12,19 +12,57 @@ export class Courses {
 
   courses = signal<Course[]>([]); //signal som lagrar courses, startvärde tom array
   error = signal<string | null>(null); //signal som lagrar error, startvärde null
-  filterText = signal("");
-  filteredCourses = computed(() => {
-    const filter = this.filterText().trim().toLowerCase();
-    if (!filter) return this.courses();
 
-    return this.courses().filter(c => 
+
+  filterText = signal(""); //signal som lagrar sökfras
+
+  //signal som lagrar filtrerat värde
+  filteredCourses = computed(() => {
+    const filter = this.filterText().trim().toLowerCase();  //Inmatad sökfras (inga mellanstag, gemener)
+    if (!filter) return this.courses(); //Om ingen sökfras, visa alla kurser
+
+    //Returner kursnamn och kurskoder som inkluderar sökfras
+    return this.courses().filter(c =>
       c.courseName.toLowerCase().includes(filter) ||
-        c.courseCode.toLowerCase().includes(filter)
+      c.courseCode.toLowerCase().includes(filter)
     );
   })
 
+
+
+  sortChoice = signal("normal");
+
+
+  sortCourses = computed(() => {
+    const coursesCopy = [...this.filteredCourses()]; //Allakurser från filteredCourses i ny array
+    const value = this.sortChoice();  //Det val vi gjort i listan
+
+    if (value === "byCode") {
+      return coursesCopy.sort((a, b) => a.courseCode.localeCompare(b.courseCode));  //sortera på kurskod
+    }
+
+    if (value === "byName") {
+      return coursesCopy.sort((a, b) => a.courseName.localeCompare(b.courseName));  //sortera på kursnamn
+    }
+
+    if (value === "byPoints") {
+      return coursesCopy.sort((a, b) => a.points - b.points);  //sortera på poäng
+    }
+
+    if (value === "bySubject") {
+      return coursesCopy.sort((a, b) => a.subject.localeCompare(b.subject));  //sortera på poäng
+    }
+
+    return coursesCopy;   //Annars orginalordning
+
+  });
+
+
+
+
   coursesService = inject(CoursesService);
 
+  //Körs när komponenten startar
   ngOnInit() {
     this.loadCourses();
   }
